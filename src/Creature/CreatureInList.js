c49203492b4ae909f42489e3ec17a02be213e69b
bradskimitch <bradley.stephen.mitchell.95@gmail.com>
2019-02-04 00:25:38 +0000

    Creatures can CRUD

--- src/Creature/CreatureInList.js
+++ src/Creature/CreatureInList.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import axios from 'axios';
 class CreatureListButton extends Component {
     deleteEntry = () => {
-        axios.delete('http://localhost:8080/SoloProject/rest/solo/item/json/' + this.props.Id)
+        axios.delete('http://localhost:8080/SoloProject/rest/solo/creature/json/' + this.props.Id)
             .then(res => {
                 this.props.action();
             });
+
+    }
+
+    updateItem = (e) => {
+        e.preventDefault();
+        axios.put('http://localhost:8080/SoloProject/rest/solo/creature/json/' + this.props.Id, {
+            creatureName: this.refs.creatureName.value,
+            creatureAC: this.refs.creatureAC.value,
+            creatureHP: this.refs.creatureHP.value,
+            creatureSpeed: this.refs.creatureSpeed.value
+        }).then(response => {
+            this.props.action();
+        });
+        this.refs.creatureName.value = '';
+        this.refs.creatureAC.value = '';
+        this.refs.creatureHP.value = '';
+        this.refs.creatureSpeed.value = '';
 
     }
 
--- src/Creature/CreatureList.js
+++ src/Creature/CreatureList.js
@@ -37,19 +37,34 @@ class CreatureList extends Component {
             })
     }
 
+    addItem = (e) => {
+        e.preventDefault();
+        axios.post(`http://localhost:8080/SoloProject/rest/solo/creature/json`, {
+            creatureName: this.refs.creatureName.value,
+            creatureAC: this.refs.creatureAC.value,
+            creatureHP: this.refs.creatureHP.value,
+            creatureSpeed: this.refs.creatureSpeed.value
+        }).then(response => {
+            this.update();
+        });
+        this.refs.creatureName.value = null;
+        this.refs.itemType.value = null;
+        this.refs.itemRarity.value = null;
+        this.refs.itemAttunement.value = null;
+    }
+
     render() {
         let elements = [];
         let objects = this.state.creatures;
         for (let i = 0; i < this.state.creatures.length; i++) {
             elements.push(
                 <CreatureListButton
-                    action ={this.handler}
-                    Id ={this.state.creatures[i].creatureId}
-                    Name ={this.state.creatures[i].creatureName}
-                    AC ={this.state.creatures[i].creatureAC}
-                    HP ={this.state.creatures[i].creatureHP}
-                    Speed ={this.state.creatures[i].creatureSpeed}
-                    STR = {this.state.creatures[i].creatureSTR}
+                    action={this.handler}
+                    Id={this.state.creatures[i].id}
+                    Name={this.state.creatures[i].creatureName}
+                    AC={this.state.creatures[i].creatureAC}
+                    HP={this.state.creatures[i].creatureHP}
+                    Speed={this.state.creatures[i].creatureSpeed}
                 />
             );
         }
@@ -58,6 +73,17 @@ class CreatureList extends Component {
                 <header>
                     <input ref="userInput" type="text" placeholder="Enter Creature Name" onChange={this.update} />
                 </header>
+                <form className='itemForm' onSubmit={this.addItem}>
+                    <fieldset>
+                        <legend>New Creature</legend>
+                        <input ref="creatureName" type="text" placeholder="Enter Creature Name" />
+                        <input ref="creatureAC" type="text" placeholder="Enter Creature AC" />
+                        <input ref="creatureHP" type="text" placeholder="Enter Item Rarity" />
+                        <input ref="creatureSpeed" type="text" placeholder="Enter Item Attunement Needs" />
+                        <button type='submit'>Submit</button>
+
+                    </fieldset>
+                </form>
                 {elements}
             </div>
         );
